40b0815529c82d9299a09085564b708dc0ffaac5
VThang51 <118606753+VThang51@users.noreply.github.com>
2023-03-26 17:26:09 +0700

    Update sudo.sh

--- General/sudo.sh
+++ General/sudo.sh
@@ -5,5 +5,7 @@ sudo -E apt-get -qq update
 sudo -E apt-get -qq install bc build-essential zip curl libstdc++6 git wget python gcc clang libssl-dev repo rsync flex curl bison aria2 python2.7 python-pip
 git config --global user.name "VThang51"
 git config --global user.email "vietthang0511.2@gmail.com"
-sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
-sudo chmod a+rx /usr/local/bin/repo
+mkdir ~/bin
+PATH=~/bin:$PATH
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+rx ~/bin/repo
