6b0ae7da9c39db9bd78b526f8f355b7599ae11c7
VThang51 <118606753+VThang51@users.noreply.github.com>
2023-04-08 22:32:34 +0700

    Update sudo.sh

--- sh/sudo.sh
+++ sh/sudo.sh
@@ -9,3 +9,6 @@ mkdir ~/bin
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 sudo ln -sf ~/bin/repo /usr/bin/repo
+sudo wget -qO /usr/local/bin/ninja.gz https://github.com/ninja-build/ninja/releases/latest/download/ninja-linux.zip
+sudo gunzip /usr/local/bin/ninja.gz
+sudo chmod a+x /usr/local/bin/ninja
