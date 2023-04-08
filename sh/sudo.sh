09340e9b7fc1776288ce094d2ae5e503b19ea76d
VThang51 <118606753+VThang51@users.noreply.github.com>
2023-04-08 22:33:48 +0700

    Update sudo.sh

--- sh/sudo.sh
+++ sh/sudo.sh
@@ -4,7 +4,7 @@ sudo rm -rf "/usr/local/share/boost"
 sudo rm -rf "$AGENT_TOOLSDIRECTORY"
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install bc build-essential zip curl libstdc++6 git wget python python2 gcc clang libssl-dev rsync flex curl bison aria2
+sudo apt-get -y install bc build-essential zip curl libstdc++6 git wget python python2 gcc clang libssl-dev rsync flex curl bison aria2 ninja-build
 mkdir ~/bin
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
