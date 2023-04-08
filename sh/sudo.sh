88645a1d2514726ff0f29c580f015289445bbb46
VThang51 <118606753+VThang51@users.noreply.github.com>
2023-04-08 11:26:55 +0700

    Update sudo.sh

--- sh/sudo.sh
+++ sh/sudo.sh
@@ -1,18 +1,11 @@
-echo "- Start Action"
-echo "-- Free up space"
 sudo rm -rf /usr/share/dotnet
 sudo rm -rf /opt/ghc
 sudo rm -rf "/usr/local/share/boost"
 sudo rm -rf "$AGENT_TOOLSDIRECTORY"
-echo "-- Update Link and Pakage"
 sudo apt-get -y update
 sudo apt-get -y upgrade
-echo "-- Install the necessary packages"
 sudo apt-get -y install bc build-essential zip curl libstdc++6 git wget python python2 gcc clang libssl-dev rsync flex curl bison aria2
-echo "-- Install Repo"
 mkdir ~/bin
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 sudo ln -sf ~/bin/repo /usr/bin/repo
-echo "-All Completed!"
-exit
