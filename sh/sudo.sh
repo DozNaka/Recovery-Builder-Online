969cb095a9c151a6cec33416f65ce436dec027cc
VThang51 <118606753+VThang51@users.noreply.github.com>
2023-04-09 11:18:49 +0700

    Update sudo.sh

--- sh/sudo.sh
+++ sh/sudo.sh
@@ -4,11 +4,8 @@ sudo rm -rf "/usr/local/share/boost"
 sudo rm -rf "$AGENT_TOOLSDIRECTORY"
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install bc build-essential zip curl libstdc++6 git wget python python2 gcc clang libssl-dev rsync flex curl bison aria2 ninja-build
+sudo apt-get -y install bc build-essential zip curl libstdc++6 git wget python python2 gcc clang libssl-dev rsync flex curl bison aria2 openjdk-8-jdk make
 mkdir ~/bin
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 sudo ln -sf ~/bin/repo /usr/bin/repo
-sudo wget -qO /usr/local/bin/ninja.gz https://github.com/ninja-build/ninja/releases/latest/download/ninja-linux.zip
-sudo gunzip /usr/local/bin/ninja.gz
-sudo chmod a+x /usr/local/bin/ninja
