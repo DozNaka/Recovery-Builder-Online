9216ff04ebce82fafd4b4347da3d9ce1dc70b899
Nguyễn Viết Thắng <118606753+VThang51@users.noreply.github.com>
2023-04-23 12:41:54 +0700

    Update sudo.sh

--- sh/sudo.sh
+++ sh/sudo.sh
@@ -7,7 +7,7 @@ sudo apt-get -y install curl
 sudo apt-get -y install libstdc++6
 sudo apt-get -y install git
 sudo apt-get -y install wget
-sudo apt-get -y install python
+sudo apt-get -y install python3
 sudo apt-get -y install python2
 sudo apt-get -y install gcc
 sudo apt-get -y install clang
